93ef283c62b250eb913dd3855c466b3ca0a54852
Shafeeque Onile <mshafeequemsn@gmail.com>
2023-08-27 11:17:22 +0200

    updated the repository name

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/personal-website/',
+  base:'/shafeequeonline.github.io/',
   plugins: [
     vue(),
     vueJsx(),
